ff7dcc9b8e3bcbe98b51b681bacbf66395015b8a
Mark Hil <mark@100shapes.com>
2021-12-28 15:45:29 +0000

    Added get related artist ids function

--- run.py
+++ run.py
@@ -19,13 +19,19 @@ def get_access_token():
     
 access_token = get_access_token()
 
-# Get artist
-def get_artist():
-    pass
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+# r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
+
 
 # Get related artists
-def get_related_artists():
-    pass
+def get_related_artists(artist_id):
+    related_artists = requests.get(base_url + 'artists/' + artist_id + '/related-artists', headers=headers).json()
+    related_artist_ids = [artist['id'] for artist in related_artists['artists']]
+    return related_artist_ids
+
+print(get_related_artists('1bwUhKRmEkOZ1wuTnV9XjC'))
 
 # Get artist top track
 def get_artist_top_track():
