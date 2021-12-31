1122230217142fb993e1107e53c4fc2a354b5d09
Mark Hil <mark@100shapes.com>
2021-12-31 12:35:08 +0000

    Updated docker-compose to read env vars from .env

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,13 +1,27 @@
 from flask import Flask
 from flask_cors import CORS
+import requests
+import os
+
+# from dotenv import dotenv_values
+# config = dotenv_values(".env")
+
+auth_url = 'https://accounts.spotify.com/api/token'
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
 
-    @app.route('/')
+    @app.route('/authenticate')
     def index():
-        return "Curator API"
+        auth_response = requests.post(auth_url, {
+            'grant_type': 'client_credentials',
+            'client_id' : os.environ['CLIENT_ID'],
+            'client_secret': os.environ['CLIENT_SECRET']            
+        })
+        auth_response_data = auth_response.json()
+        access_token = auth_response_data['access_token']
+        return access_token
 
     @app.errorhandler(404)
     def page_not_found(error):
